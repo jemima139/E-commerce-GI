82183c0641381c21468b9d69082931d56c401f0c
jemima139 <165708488+jemima139@users.noreply.github.com>
2024-05-15 08:47:32 -0400

    Comments updated

--- contact.js
+++ contact.js
@@ -1,14 +1,18 @@
+//This function takes an event object as a parameter, which represents the form submission event.
 function handleSubscribe(event) {
+//It prevents the default behavior of form submission, which is to reload the page.
     event.preventDefault(); 
 
-    
+    //fetches references to various form elements by their IDs.
     var name = document.getElementById('name');
     var phone = document.getElementById('phone');
+
     var email = document.getElementById('email');
     var dropdown = document.getElementById('dropdown');
 
     
     var isValid = true;
+    // It checks if the name field is empty. If it is, it displays an error message and sets isValid to false. Otherwise, it clears any existing error message.
     if (!name.value.trim()) {
         document.getElementById('nameError').innerText = 'Name is required';
         isValid = false;
@@ -29,7 +33,7 @@ function handleSubscribe(event) {
     } else {
         document.getElementById('emailError').innerText = '';
     }
-
+//It checks if a value is selected in the dropdown. If not, it displays an error message and sets isValid to false.
     if (!dropdown.value) {
         document.getElementById('dropdownError').innerText = 'Please select a product type';
         isValid = false;
@@ -37,7 +41,7 @@ function handleSubscribe(event) {
         document.getElementById('dropdownError').innerText = '';
     }
 
-   
+//If all validation checks pass (isValid remains true), the form is submitted.   
     if (isValid) {
         event.target.submit();
     }
